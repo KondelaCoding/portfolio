ee6b494e06570adcca8610995c8ceb2a2046f259
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-09-05 22:07:33 +0200

    Add logic to transition in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,47 @@ import "./About.css";
 import { ArrowRight, ArrowDownRight, ArrowDownLeft } from "react-feather";
 import { useState, useEffect } from "react";
 
+const data = [
+  {
+    title: "bio",
+    content:
+      "A passionate Frontend Developer and Visual Designer with a keen eye for aesthetics and a love for creating intuitive, user-friendly interfaces. With expertise in HTML, CSS, JavaScript, and popular frameworks like React, I bring designs to life with clean, efficient code. My background in visual design enhances my ability to craft engaging digital experiences that are not only functional but also visually appealing. I am dedicated to staying updated with the latest trends and technologies in both frontend development and design, constantly striving to create seamless and beautiful user experiences.",
+  },
+  {
+    title: "career",
+    content:
+      "I have over 5 years of experience in frontend development and visual design. I have worked on a wide range of projects, from small business websites to large-scale web applications. I have experience working with clients from various industries, including e-commerce, healthcare, and education. I am proficient in HTML, CSS, JavaScript, and popular frontend frameworks like React, Angular, and Vue. I am also skilled in design tools like Adobe Photoshop, Illustrator, and XD.",
+  },
+  {
+    title: "personal life",
+    content:
+      "Outside of work, I enjoy spending time with my family and friends. I am an avid reader and love exploring new books and authors. I am also passionate about travel and enjoy visiting new places and experiencing different cultures. I am a food enthusiast and love trying new cuisines and recipes. I am a nature lover and enjoy spending time outdoors, hiking, and camping. I am a fitness enthusiast and enjoy staying active through yoga, running, and weightlifting.",
+  },
+  {
+    title: "achievements",
+    content:
+      "Throughout my career, I have received several awards and accolades for my work in frontend development and visual design. I have been recognized for my creativity, innovation, and attention to detail. I have also received positive feedback from clients and colleagues for my professionalism, communication skills, and ability to meet deadlines. I am proud of the projects I have worked on and the impact they have had on users and businesses.",
+  },
+];
+
 export default function About() {
   const [activeSection, setActiveSection] = useState("bio");
   const [arrowSize, setArrowSize] = useState({ size: 80, stroke: 1.8 });
+  const [isTransition, setIsTransition] = useState(false);
 
   const handleMenuClick = (section) => {
     setActiveSection(section);
   };
+
+  useEffect(() => {
+    console.log("change");
+    setIsTransition(true);
+
+    setTimeout(() => {
+      console.log("transition-ended");
+      setIsTransition(false);
+    }, 1000);
+  }, [activeSection]);
 
   useEffect(() => {
     if (window.innerWidth < 1194) {
@@ -23,7 +57,7 @@ export default function About() {
       <div className="container">
         <div className="content">
           <div className="title">
-            <h1>{activeSection}</h1>
+            <h1 className={isTransition ? "animate-about" : ""}>{activeSection}</h1>
             <ArrowDownRight
               size={arrowSize.size}
               strokeWidth={arrowSize.stroke}
@@ -31,13 +65,8 @@ export default function About() {
               strokeLinejoin="flat"
             />
           </div>
-          <p>
-            A passionate Frontend Developer and Visual Designer with a keen eye for aesthetics and a love for creating
-            intuitive, user-friendly interfaces. With expertise in HTML, CSS, JavaScript, and popular frameworks like
-            React, I bring designs to life with clean, efficient code. My background in visual design enhances my
-            ability to craft engaging digital experiences that are not only functional but also visually appealing. I am
-            dedicated to staying updated with the latest trends and technologies in both frontend development and
-            design, constantly striving to create seamless and beautiful user experiences.
+          <p className={isTransition ? "animate-about" : ""}>
+            {data.find((item) => item.title === activeSection).content}
           </p>
           <div
             className="learn-more-mobile"
